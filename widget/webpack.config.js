0dccb30c0ffb1e0e308ec4699e0b0381e551118f
Mike McGrath <mike@mikemcgrathonline.us>
2020-03-24 23:36:52 -0400

    Commits

--- widget/webpack.config.js
+++ widget/webpack.config.js
@@ -1,31 +1,31 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const path = require("path");
-const PostCompile = require("post-compile-webpack-plugin");
-const spawn = require("cross-spawn");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+const PostCompile = require('post-compile-webpack-plugin')
+const spawn = require('cross-spawn')
 
-const APP_DIR = path.resolve(__dirname, "src");
-const BUILD_DIR = path.resolve(__dirname, "build");
+const APP_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'build')
 
 module.exports = {
   entry: {
-    main: APP_DIR + "/index.js"
+    main: APP_DIR + '/index.js',
   },
   output: {
-    library: "PromptliWidget",
-    libraryTarget: "umd",
+    library: 'PromptliWidget',
+    libraryTarget: 'umd',
     umdNamedDefine: true,
-    filename: "promptli.js",
-    publicPath: "/static/widget",
-    path: BUILD_DIR
+    filename: 'promptli.js',
+    publicPath: '/static/widget',
+    path: BUILD_DIR,
   },
   resolve: {
     alias: {
-      react: "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat"
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
       // Must be below test-utils
-    }
+    },
   },
   module: {
     rules: [
@@ -33,30 +33,26 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
-    new PostCompile(() => {
-      console.log("Building Widget");
-      spawn("yarn", "build", {});
-    })
   ],
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
-  }
-};
+    minimizer: [new UglifyJsPlugin()],
+  },
+}
