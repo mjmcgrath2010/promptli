879447d9936b9dde3e181f72f4750ab2aeff214d
Mike McGrath <mike@mikemcgrathonline.us>
2020-04-01 12:45:39 -0400

    Add text from dashboard

--- app/components/dashboard/WidgetEditor/index.js
+++ app/components/dashboard/WidgetEditor/index.js
@@ -1,10 +1,64 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import DashboardLayout from '../../../pages/dashboard/design'
+import styled from 'styled-components'
+
+const IFrameEditor = styled.iframe`
+  width: 60vw;
+  height: calc(90vh - 70px);
+  border: 3px solid gray;
+  border-radius: 5px;
+  position: absolute;
+  right: 2em;
+  top: 100px;
+`
+let iframeRef
 
 const WidgetEditor = props => {
+  const [title, setTitle] = useState('Hello')
+  useEffect(() => {
+    updateEditor()
+  }, [])
+
+  const updateEditor = () => {
+    const document = iframeRef.contentDocument
+    const documentContents = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Widget Preview</title>
+            <style>
+            </style>
+          </head>
+          <body>
+            <div id="editor" />
+            <script src="${window.location.origin}/static/widget/promptli.js"></script>
+            <script type="text/javascript">
+              window.PromptliWidget && window.PromptliWidget.init({ title: '${title}' }, 'editor')
+            </script>
+          </body>
+          </html>
+        `
+
+    document.open()
+    document.write(documentContents)
+    document.close()
+  }
   return (
     <Fragment>
-      <div>WidgetEditor</div>
+      <section className="result">
+        <input
+          type="text"
+          onChange={e => {
+            setTitle(e.target.value)
+            updateEditor()
+          }}
+        />
+        <IFrameEditor ref={ref => (iframeRef = ref)} />
+      </section>
     </Fragment>
   )
 }
--- app/pages/dashboard/design.js
+++ app/pages/dashboard/design.js
@@ -6,56 +6,12 @@ import styled from 'styled-components'
 import withAuth from '../../lib/withAuth'
 import { dashboardActions } from '../../actions/index'
 import DashboardLayout from '../../components/dashboard/layouts'
+import WidgetEditor from '../../components/dashboard/WidgetEditor'
 
-const IFrameEditor = styled.iframe`
-  width: 60vw;
-  height: calc(90vh - 70px);
-  border: 3px solid gray;
-  border-radius: 5px;
-  position: absolute;
-  right: 2em;
-  top: 70px;
-`
 class Design extends Component {
   static defaultProps = {}
   static propTypes = {}
-  static async getInitialProps() {
-    return {
-      design: '',
-    }
-  }
-  componentDidMount() {
-    this.runCode()
-  }
-
-  runCode = () => {
-    const iframe = this.iframe
-    const document = iframe.contentDocument
-    const documentContents = `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Widget Preview</title>
-            <style>
-            </style>
-          </head>
-          <body>
-            <div id="editor" />
-            <script src="${window.location.origin}/static/widget/promptli.js"></script>
-            <script type="text/javascript">
-              window.PromptliWidget && window.PromptliWidget.init({ title: 'hello from dashboard' }, 'editor')
-            </script>
-          </body>
-          </html>
-        `
-
-    document.open()
-    document.write(documentContents)
-    document.close()
-  }
+  componentDidMount() {}
 
   render() {
     return (
@@ -64,9 +20,7 @@ class Design extends Component {
           <title>Dashboard</title>
         </Head>
         <div>Design</div>
-        <section className="result">
-          <IFrameEditor ref={ref => (this.iframe = ref)} />
-        </section>
+        <WidgetEditor />
       </DashboardLayout>
     )
   }
