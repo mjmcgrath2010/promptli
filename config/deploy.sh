2835a9f256ede97ceb749028eb75ab8e93e742b0
Mike McGrath <mike@mikemcgrathonline.us>
2020-04-21 14:30:07 -0400

    New build

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,9 +4,12 @@ echo "Build Started"
 rm -rf ./app/static/widget
 cd widget
 yarn install
+wait
 yarn build
+wait
 cp -vr ./build ../app/static/widget
 cd ../app
 yarn install
+wait
 yarn build
 echo "Build Complete"
