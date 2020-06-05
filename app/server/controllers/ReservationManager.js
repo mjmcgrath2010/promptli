6c51954eb7b81c3a488b2aa0fbfc3fda2307f523
Mike McGrath <mike@mikemcgrathonline.us>
2020-06-05 14:39:39 -0400

    Make reservations possible, account for number of parking spaces that can be used at a given time

---
+++ app/server/controllers/ReservationManager.js
@@ -0,0 +1,84 @@
+const Reservation = require('../models/Reservations')
+const Account = require('../models/Account')
+const Item = require('../models/Item')
+
+exports.create = (req, res) => {
+  const { startDate, endDate, customer, items, totalCost } = req.body
+  const _id = req.params.id
+
+  const reservationParams = {
+    startDate,
+    endDate,
+    customer,
+    items,
+    totalCost,
+    account: _id,
+  }
+  /*
+   *  TODO:
+   *   = Process stripe charges
+   *   = Send Confirmation Emails
+   */
+
+  Item.find()
+    .where('_id')
+    .in(items)
+    .then(items => {
+      const minQuantity = Math.min.apply(
+        Math,
+        items.map(item => item.quantity)
+      )
+
+      Account.findById(_id, err => {
+        if (err) {
+          return res.sendStatus(404)
+        }
+
+        Reservation.find({
+          account: _id,
+        })
+          .where('startDate')
+          .gte(startDate)
+          .where('startDate')
+          .lte(endDate)
+          .where('endDate')
+          .lte(endDate)
+          .where('endDate')
+          .gte(startDate)
+          .where('items')
+          .in(items)
+          .then(reservations => {
+            let appointment
+            if (reservations.length >= minQuantity) {
+              return res.json({ error: 'reservation cannot be booked' })
+            }
+
+            appointment = new Reservation(reservationParams)
+            appointment
+              .save()
+              .then(result => {
+                result.populate('items', (err, doc) => res.json(doc))
+              })
+              .catch(e => res.send(e))
+          })
+          .catch(e => res.send(e))
+      })
+    })
+    .catch(e => res.send(e))
+}
+
+exports.getReservationsByDate = (req, res, next) => {
+  /* TODO:
+   *   - Query Reservation by day
+   *
+   */
+  next()
+}
+
+exports.getReservationsByDateRange = (req, res, next) => {
+  /* TODO:
+   *   - Query Reservation by date range
+   *
+   */
+  next()
+}
--- app/server/routes/ServiceManager.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/ServiceManager')
-
-router.route('/:id').post(controller.create)
-
-module.exports = router
