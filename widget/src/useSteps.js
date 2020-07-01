9e5ac7b0b5c75ecfff984563158bca6c2a896f2d
Mike McGrath <mike@mikemcgrathonline.us>
2020-07-01 19:22:57 -0400

    Cleanup

--- widget/src/useSteps.js
+++ widget/src/useSteps.js
@@ -3,21 +3,11 @@ import { useState } from 'preact/hooks'
 const getIndexById = (arr, matchId) => arr.findIndex(({ id }) => id === matchId)
 
 const useSteps = ({ steps: stepsProp, initialStep = 0 }) => {
-  if (process.env.NODE_ENV !== 'production') {
-    if (!Array.isArray(stepsProp) && !Number.isInteger(stepsProp)) {
-      error('useStep: You must specify either an array or an integer for `steps`')
-    }
-  }
   // Convert steps to an array if it is a number.
   const steps = typeof stepsProp === 'number' ? new Array(stepsProp).fill({}) : stepsProp
   // Compute initialStepIndex in case an id is passed vs an index.
   const initialStepIndex = typeof initialStep === 'number' ? initialStep : getIndexById(steps, initialStep)
 
-  if (process.env.NODE_ENV !== 'production') {
-    if (typeof initialStep === 'string' && initialStepIndex === -1) {
-      error(`useStep: id of "${initialStep}" specified in initialStep not found in steps`)
-    }
-  }
   // Setup state.
   const [index, setStep] = useState(initialStepIndex)
 
