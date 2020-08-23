bb42ffac5bc0612556f56c4ca70cf5ed01cdf5f0
Mike McGrath <mike@mikemcgrathonline.us>
2020-08-23 08:21:24 -0400

    Pushing Up

--- app/next.config.js
+++ app/next.config.js
@@ -7,11 +7,8 @@ module.exports = {
     TEST_WIDGET_ID: process.env.TEST_WIDGET_ID,
     TEST_BUSINESS_IDENTIFIER: process.env.TEST_BUSINESS_IDENTIFIER,
   },
-  serverRuntimeConfig: {},
   publicRuntimeConfig: {
     localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string' ? process.env.LOCALE_SUBPATHS : 'none',
-    TEST_WIDGET_ID: process.env.TEST_WIDGET_ID,
-    TEST_BUSINESS_IDENTIFIER: process.env.TEST_BUSINESS_IDENTIFIER,
     staticFolder: '/static',
   },
   webpack: (config, options) => {
