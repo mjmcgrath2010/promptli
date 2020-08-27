2081a2708c11f25f65a1a285a760bec7798fb667
Mike McGrath <mike@mikemcgrathonline.us>
2020-08-27 12:26:46 -0400

    Fix test page?

--- app/pages/test/widget.js
+++ app/pages/test/widget.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Head from 'next/head'
-import { Container } from '@material-ui/core'
 
 class WidgetTest extends Component {
   static defaultProps = {}
   static propTypes = {}
 
   componentDidMount() {
-    const el = document.getElementById('demo')
-    window &&
-      el &&
-      window.PromptliWidget.init(
-        {
-          identifier: process.env.TEST_BUSINESS_IDENTIFIER,
-          widgetId: process.env.TEST_WIDGET_ID,
-        },
-        'demo'
-      )
+    window.location.replace(
+      `/static/test/widget.html?identifier=${process.env.TEST_BUSINESS_IDENTIFIER}&widgetId=${process.env.TEST_WIDGET_ID}`
+    )
   }
 
   render() {
-    return (
-      <div>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Widget Integrations | Promptli</title>
-        </Head>
-        <Container>
-          <div id="demo"></div>
-        </Container>
-      </div>
-    )
+    return <div></div>
   }
 }
 
